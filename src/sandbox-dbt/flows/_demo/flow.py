3ae88210d450c9d8364e1d2caffcf6d17dca86b2
griggz <waynewgrigsby@gmail.com>
2024-09-10 08:52:48 -0400

    updates to add dbt demo

--- src/sandbox-dbt/flows/_demo/flow.py
+++ src/sandbox-dbt/flows/_demo/flow.py
@@ -1,106 +1,67 @@
-"""
-This is a demo flow that simulates taking a walk and storing the walk details in a SQLite database. 
-The flow also triggers a dbt build to update the models based on the latest walk data.
-
-source: https://www.prefect.io/blog/dbt-and-prefect
-"""
+from prefect import flow, task
+from prefect_github import GitHubRepository
+from prefect_dbt.cli.commands import trigger_dbt_cli_command
+from pathlib import Path
+from enum import Enum
+from typing import Optional
 import os
-import sqlite3
-from sandbox.flows._demo.flow import take_a_walk
-from prefect_dbt.cli.commands import DbtCoreOperation, DbtCliProfile
-from prefect import task, flow
+
 from pathlib import Path
 
-profiles_path = Path(os.path.abspath(".dbt/profiles.yml"))
 
-# # Define the DbtCliProfile with the full path to the profiles.yml file
-# dbt_profile = DbtCliProfile(
-#     name="sqlite_profile",  # Name of your dbt profile
-#     target="dev",           # The target environment
-#     profiles_dir=profiles_path,  # Full directory path to profiles.yml
-#     # project_dir="path_to_your_dbt_project",  # Your dbt project directory
-#     overwrite=True,         # Whether to overwrite the profile or not
-#     target_configs={        # The required target configurations for SQLite
-#         "type": "sqlite",
-#         "threads": 1,
-#         "database": "dev.db",  # Path to your SQLite database
-#         "schema": "main"  
-#     }
-
-# )
-
-# Define a function to initialize the SQLite table
-def initialize_db():
-    # Construct the path to the database directory
-    db_dir = Path(".").resolve().parent.parent / "src/core/database"
-    # Ensure the directory exists (create it if it doesn't)
-    db_dir.mkdir(parents=True, exist_ok=True)
-    # Define the path for the SQLite database file
-    db_path = db_dir / "my_database.db"
-    conn = sqlite3.connect(db_path)
-    cursor = conn.cursor()
-
-    # Create a table to store walk details
-    cursor.execute("""CREATE TABLE IF NOT EXISTS walks (
-                        id INTEGER PRIMARY KEY AUTOINCREMENT,
-                        city TEXT,
-                        near TEXT,
-                        cafe_name TEXT,
-                        park_name TEXT
-                      )""")
-    conn.commit()
-    conn.close()
+class Branch(Enum):
+    MAIN = "duckdb"
+    FAIL = "model_error"
 
 
-# Task to insert walk data into the SQLite database
-@task
-def store_walk_in_db(walk_data: dict):
-    # Construct the path to the database directory
-    db_dir = Path(".").resolve().parent.parent / "src/core/database"
-    # Ensure the directory exists (create it if it doesn't)
-    db_dir.mkdir(parents=True, exist_ok=True)
-    # Define the path for the SQLite database file
-    db_path = db_dir / "my_database.db"
-    conn = sqlite3.connect(db_path)
-    cursor = conn.cursor()
-
-    # Insert the walk details into the walks table
-    cursor.execute(
-        """INSERT INTO walks (city, near, cafe_name, park_name)
-                      VALUES (?, ?, ?, ?)""",
-        (
-            walk_data["city"],
-            walk_data["near"],
-            walk_data["cafe"]["name"],
-            walk_data["park"]["name"],
-        ),
-    )
-
-    conn.commit()
-    conn.close()
-
-@task
-def trigger_dbt_flow() -> str:
-    result = DbtCoreOperation(
-        commands=["dbt build -t dev"],       # dbt commands to run
-        profiles_dir=str(profiles_path),  # Programmatically get the path
-    ).run()
-    return result
+@task(name="clone the repo")
+def clone_dbt_repo(destination_path: str, reference: str = "duckdb"):
+    """Clones public repo. Specify branch or reference and where to store it locally"""
+    dbt_repo_url = "https://github.com/seanpwlms/jaffle_shop_duckdb.git"
+    repository = GitHubRepository(repository_url=dbt_repo_url, reference=reference)
+    repository.get_directory(local_path=destination_path)
 
 
-# Flow to simulate a walk and store it in the database
+def dbt_deps():
+    """install dependencies"""
+    trigger_dbt_cli_command(command="dbt deps", project_dir="./dbt", profiles_dir=".")
+
+
+def get_dbt_paths(profiles_dir: str = "./dbt", project_dir: str = "./dbt"):
+    """resolve paths and validate profiles.yml is found"""
+    profiles_dir = str(Path(profiles_dir).resolve())
+    project_dir = str(Path(project_dir).resolve())
+    assert os.path.exists(f"{profiles_dir}/profiles.yml")
+    return profiles_dir, project_dir
+
+
 @flow
-def dbt_injections():
-    # Initialize the database
-    initialize_db()
+def dbt_build_flow(
+    branch: Optional[Branch] = Branch.MAIN, destination_path: Optional[str] = "./dbt"
+):
+    """Clone and run dbt project"""
+    if not os.path.isdir(destination_path):
+        clone_dbt_repo(destination_path=destination_path, reference=branch)
 
-    # Simulate taking a walk and store the results
-    my_walk = take_a_walk(city="Lyon, France", near="Place de Tapis")
-    store_walk_in_db(my_walk)
-
-    # Trigger dbt to build models using the latest walk data
-    dbt = trigger_dbt_flow()
+    profiles_dir, project_dir = get_dbt_paths()
+    trigger_dbt_cli_command(
+        command="dbt seed",
+        project_dir=project_dir,
+        profiles_dir=profiles_dir,
+    )
+    trigger_dbt_cli_command(
+        command="dbt deps",
+        project_dir=project_dir,
+        profiles_dir=profiles_dir,
+    )
+    trigger_dbt_cli_command(
+        command="dbt build",
+        project_dir=project_dir,
+        profiles_dir=profiles_dir,
+        create_summary_artifact=True,
+        summary_artifact_key="dbt-build-task-summary",
+    )
 
 
 if __name__ == "__main__":
-    dbt_injections()
+    dbt_build_flow()
