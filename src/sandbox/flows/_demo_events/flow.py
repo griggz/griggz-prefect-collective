b6ac39b8c5d4c99c2405b161dff54183f560b70b
griggz <waynewgrigsby@gmail.com>
2024-10-03 13:43:21 -0400

    updates to events demo

--- src/sandbox/flows/_demo_events/flow.py
+++ src/sandbox/flows/_demo_events/flow.py
@@ -2,7 +2,7 @@ from prefect import flow, task
 from prefect.logging import get_run_logger
 from prefect.context import get_run_context
 from prefect.events import emit_event
-from prefect.runtime import task_run
+from prefect.runtime import task_run, flow_run
 from prefect.artifacts import create_markdown_artifact
 from pydantic import BaseModel
 from controlflow import Agent, Task
@@ -41,7 +41,7 @@ def validate_data(data):
 
 
 # Task for auditing and compliance
-@task
+
 def generate_audit_data(events):
     logger = get_run_logger()
     logger.info("Audit Report: Generated compliance report.")
@@ -53,16 +53,28 @@ def generate_audit_data(events):
 def log_artifact(markdown, title):
     logger = get_run_logger()
     logger.info("Audit Report: Logged compliance report as artifact.")
+    emit_event(
+        event="Report Completed",
+        resource={"prefect.resource.id": flow_run.id},
+    )
     create_markdown_artifact(
         key="audit-report",
         markdown=markdown,
         description=title,
     )
 
+def send_report():
+    logger = get_run_logger()
+    logger.info("Audit Report: Sent compliance report.")
+    emit_event(
+        event="Report Sent",
+        resource={"prefect.resource.id": flow_run.id},
+    )
 
-# Main flow
-@flow
+
+@flow()
 def governance_flow(data=[1, 2, 3]):
+    report = None
     logger = get_run_logger()
 
     initiate_validations = logger.info("Governance validations flow initiated.")
@@ -85,9 +97,10 @@ def governance_flow(data=[1, 2, 3]):
 
     report = Task(
         """
-            Lets take the data and write a compliance report. I want the report to be a paragraph long and
-            written in markdown, and include the details of the data and the flow run status. The context I'm giving you is output of a 
-            prefect flow and data. 
+            Lets take the data and write a compliance report. I want the report 
+            to be a paragraph long and written in markdown, and include the 
+            details of the data and the flow run status. The context I'm 
+            giving you is output of a prefect flow and data. 
         """,
         context=dict(
             data=audit_data,
